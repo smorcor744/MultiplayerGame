9b10511f87b1966de018cb44343355c10e86efce
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2026-01-09 20:31:03 +0100

    Add multiple player spawn points and random assignment
    Added four distinct spawn point nodes to the map scene, each grouped as 'PuntosSpawn'. Updated the player spawning logic to randomly assign a spawn point to each connecting peer and remove it from the group to prevent reuse. The spawn function now receives both peer ID and position, ensuring players spawn at unique locations.

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @export var player_scene: PackedScene
 
-
 @onready var multiplayer_spawner: MultiplayerSpawner = $Spawners/MultiplayerSpawner
 @onready var players_container: Node2D = $Players
 
@@ -15,40 +14,44 @@ func _ready():
 		multiplayer.peer_connected.connect(_on_peer_connected)
 		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 		
-
 		var all_peers = multiplayer.get_peers()
-		all_peers.append(1) 
+		all_peers.append(1)
 		
 		for peer_id in all_peers:
 			_on_peer_connected(peer_id)
 
 
 func _spawn_player_function(data) -> Node:
-	var peer_id = data
+	var peer_id = data.id   
+	var spawn_pos = data.pos 
+	
 	var player = player_scene.instantiate()
 	
-	player.name = str(peer_id) 
-	
-	player.position = Vector2(100, 100) 
-	
+	player.name = str(peer_id)
+	player.position = spawn_pos  
 	player.set_multiplayer_authority(peer_id)
 	
-	print("Spawn function ejecutada para ID: ", peer_id)
+	print("Spawn ejecutado para ID: ", peer_id, " en pos: ", spawn_pos)
 	return player
 
-# Cuando alguien entra (o al iniciar el mapa para los que ya están)
 func _on_peer_connected(id: int):
-	print("Solicitando spawn para ID: ", id)
-	multiplayer_spawner.spawn(id)
+	# Obtenemos todos los puntos de spawn
+	var puntos = get_tree().get_nodes_in_group("PuntosSpawn")
+	
+	# Elegimos uno al azar (o podrías usar un contador para ir en orden)
+	var punto_elegido = puntos.pick_random()
+	punto_elegido.remove_from_group("PuntosSpawn")
+	var datos_spawn = {
+		"id": id,
+		"pos": punto_elegido.global_position
+	}
+	
+	multiplayer_spawner.spawn(datos_spawn)
 
-# Cuando alguien se desconecta
 func _on_peer_disconnected(id: int):
 	print("Jugador desconectado: ", id)
 	if players_container.has_node(str(id)):
 		players_container.get_node(str(id)).queue_free()
-
-
-
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
